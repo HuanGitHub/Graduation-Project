f5b6099d057af9436ac7a7bba0181e0e27116ec1
HuanGitHub <601335832@qq.com>
2018-03-27 01:33:17 -0700

    add qt new untitled acom

--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -2,14 +2,74 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QTcpSocket>
+#include <qfile.h>
+#include <qjsonarray.h>
+#include <qjsondocument.h>
+#include <qjsonobject.h>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Qser = new QTcpServer(this);
-    Qser->listen(QHostAddress("127.0.0.1"),8080);
-    connect(Qser, SIGNAL(newConnection()), this, SLOT(newClient()));
+//    Qser = new QTcpServer(this);
+//    Qser->listen(QHostAddress(Ser_IP),Ser_Port);
+ //   connect(Qser, SIGNAL(newConnection()), this, SLOT(newClient()));
+    QFile file("/home/zhanghuan/data.json");
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug()<<"Can't open the file!"<<endl;
+    }
+    QByteArray line = file.readAll();
+    QString str(line);
+ //   qDebug()<< str;
+    QJsonParseError jsonError;
+    doucment = QJsonDocument::fromJson(line, &jsonError);
+    if (!doucment.isNull() && (jsonError.error == QJsonParseError::NoError)) {
+         if (doucment.isObject()) {
+            QJsonObject object = doucment.object();
+            if (object.contains("HeWeather6")) {
+                      QJsonValue value = object.value("HeWeather6");
+                 //     qDebug()<< value;
+                      if(value.isArray())
+                      {
+                          QJsonArray array = value.toArray();
+                          int nSize = array.size();
+                          for (int i = 0; i < nSize; ++i)
+                          {
+                            qDebug()<< array.at(0);
+                            QJsonValue value = array.at(i);
+                            if(array.at(i).isObject())
+                            {
+                                QJsonObject object = array.at(i).toObject();
+                                if(object.contains("update"))
+                                {
+                                      QJsonValue value = object.value("update");
+                                //      qDebug()<< value;
+                                      if(value.isObject())
+                                      {
+                                          qDebug()<< value;
+                                          QJsonObject object = value.toObject();
+                                          if(object.contains("loc"))
+                                          {
+                                              QJsonValue value = object.value("loc");
+                                              qDebug()<< value;
+                                              QString str =value.toString();
+                                            //  ui->label_9->setText(str);
+                                          }
+                                      }
+                                }
+                            }
+                          }
+
+
+                      }
+                      if (value.isString()) {
+                          QString strName = value.toString();
+                          qDebug() << "HeWeather6 : " << strName;
+                      }
+                  }
+         }
+    }
 
 }
 void MainWindow::NewCon()
@@ -30,12 +90,20 @@ void MainWindow::deleteLater()
     qDebug() << "Client disconnect: ";
 
 }
+void MainWindow::checkData()
+{
+
+
+
+}
 void MainWindow::readData()
 {
     QTcpSocket* myClient = qobject_cast<QTcpSocket*>(sender());
     QByteArray data = myClient->readAll();
     qDebug()<< data;
-   // sendData(myClient, data);
+    QByteArray Hexdata = data.toHex();
+    for(int i=0;i<Hexdata.size();i++)
+        qDebug()<<Hexdata[i];
 }
 
 MainWindow::~MainWindow()
--- untitled/mainwindow.h
+++ untitled/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <QtNetwork/qtcpserver.h>
+#include <qjsondocument.h>
+
+#define Ser_IP "127.0.0.1"
+#define Ser_Port 8080
 namespace Ui {
 class MainWindow;
 }
@@ -16,7 +20,7 @@ public:
 
     ~MainWindow();
     void NewCon();
-
+    void checkData();
 private slots:
     void newClient();
     void deleteLater();
@@ -25,6 +29,8 @@ private slots:
 private:
     Ui::MainWindow *ui;
     QTcpServer *Qser;
+    QByteArray arry;
+    QJsonDocument doucment;
 
 };
 
